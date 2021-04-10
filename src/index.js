d24326f8ed4b88df0c5b928800ae65324d7ae84e
taehyeon <th05662205@gmail.com>
2021-04-11 01:03:38 +0900

    update

---
+++ src/index.js
@@ -0,0 +1,34 @@
+class Store {
+  events = {};
+  state = {};
+
+  constructor(state) {
+    this.state = state;
+  }
+
+  getState() {
+    return this.state;
+  }
+
+  on(state, fn) {
+    if (!this.events.hasOwnProperty(state)) {
+      this.events[state] = [fn];
+    } else {
+      this.events[state] = [...this.events[state], fn];
+    }
+  }
+
+  set(data) {
+    const events = this.events;
+    const keys = Object.keys(data);
+
+    for (const key of keys) {
+      if (data[key] !== this.state[key]) {
+        this.state[key] = data[key];
+        events[key].forEach((fn) => {
+          fn();
+        });
+      }
+    }
+  }
+}
