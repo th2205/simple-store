99b3e3caa1e84b610114bc8d0ca86adbf8549a77
taehyeon <th05662205@gmail.com>
2021-04-15 00:14:35 +0900

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ class Store {
 
   constructor(state) {
     this.state = state;
+    this.isExsistTimeout = false;
   }
 
   getState() {
@@ -12,31 +13,89 @@ class Store {
 
   on(state, fn) {
     if (!this.events.hasOwnProperty(state)) {
-      this.events[state] = [fn];
-    } else {
-      this.events[state] = [...this.events[state], fn];
+      this.events[state] = [];
     }
+
+    this.events[state].push(fn);
   }
 
   set(data) {
     const events = this.events;
     const keys = Object.keys(data);
 
+    let timer;
+
     for (const key of keys) {
-      if (data[key] !== this.state[key]) {
+      if (
+        data[key] !== undefined &&
+        this.state[key] !== undefined &&
+        data[key] !== this.state[key]
+      ) {
         this.state[key] = data[key];
-        events[key].forEach((fn) => {
-          fn();
-        });
+        // events[key].forEach((fn) => {
+        //   fn();
+        // });
+
+        if (!this.isExsistTimeout) {
+          this.isExsistTimeout = true;
+          timer = setTimeout(() => {
+            events[key].forEach((fn) => {
+              fn();
+            });
+            this.isExsistTimeout = false;
+          }, 10);
+        } else {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            events[key].forEach((fn) => {
+              fn();
+            });
+            this.isExsistTimeout = false;
+          }, 10);
+        }
       }
     }
   }
 }
 
+const store = new Store({
+  list: [],
+  isLoading: false
+});
 
-  if (!this.events.hasOwnProperty(state)) {
-    this.events[state] = [];
-  }
+store.on('list', () => {
+  console.log(store.getState().list);
+});
 
-  this.events[state].push(fn);
+store.on('isLoading', () => {
+  const isLoading = store.getState().isLoading;
 
+  if (isLoading) return console.log('loading');
+  console.log('is not loading');
+});
+
+async function work() {
+  store.set({
+    isLoading: true
+  });
+
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 3000);
+  });
+
+  store.set({
+    list: [12],
+    isLoading: false
+  });
+}
+// store.set({
+//   list: [1]
+// });
+
+// store.set({
+//   list: [2]
+// });
+
+work();
