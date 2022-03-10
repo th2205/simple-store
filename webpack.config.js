90a51a32fb9fb54426ac8ac6b7b1a0de7f8a7d0e
임태현 <th05662205@gmail.com>
2022-03-10 16:11:31 +0900

    feat: ie 대응

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-
+  mode: "production",
+  entry: "./src/index.js",
+  target: ["web", "es5"],
   module: {
     rules: [
       {
         test: /.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
 
   output: {
-    filename: 'simple-store.js',
+    filename: "simple-store.js",
     library: {
-      name: 'Store',
-      type: 'umd',
-      export: 'default'
-    }
-  }
+      name: "Store",
+      type: "umd",
+      export: "default",
+    },
+  },
 };
